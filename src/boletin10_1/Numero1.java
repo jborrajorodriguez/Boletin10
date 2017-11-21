d07a984ee551a266eba5bcaf5816c2304dbcb26d
Juan <Juan@Juan-PC>
2017-11-21 22:47:09 +0100

    inicio numero

---
+++ src/boletin10_1/Numero1.java
@@ -0,0 +1,20 @@
+package boletin10_1;
+/**
+ *
+ * @author Juan
+ */
+public class Numero1 {
+    private int num1;
+    
+    public Numero1(){
+        
+    }
+    
+    public void validarnum(){
+        while(num1<1&&num1>50){
+            
+        }
+            
+    }
+    
+}
