32084dbf71aede66fdcd6cefe8f6022c2c37ea0a
Juan <Juan@Juan-PC>
2017-11-21 22:46:51 +0100

    Inicio proyecto

---
+++ src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,14 @@
+package boletin10_1;
+
+/**
+ *
+ * @author Juan
+ */
+public class Boletin10_1 {
+
+    public static void main(String[] args) {
+        Adivina prueba=new Adivina();
+        prueba.adivinar();
+    }
+    
+}
